c1b30a1d57a1e940db0dcbf5a9a772b88dfcd219
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-05-05 18:05:21 +0100

    removing db

--- backend/website/static/js/utils.js
+++
@@ -1,6 +0,0 @@
-export function showHideElement($targetElement, $trigger, onEvent = "click") {
-  $trigger.on(onEvent, function () {
-    if ($targetElement.is(":visible")) $targetElement.fadeOut();
-    else $targetElement.fadeIn();
-  });
-}
