f111fe172d5489bdb2938a23fcc4f6e27312cbc0
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-05-05 18:05:28 +0100

    adding stuff

---
+++ backend/myapi/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # TODO Remover
+    path("hello-world/", views.hello_world, name="hello_world"),
+    path("getOfenseWords/", views.getOfenseWords, name="getOfenseWords"),
+    path(
+        "<int:place_id>/isFavorite",
+        views.isFavoritePlace,
+        name="isFavoritePlace",
+    ),
+]
