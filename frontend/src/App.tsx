bc29ad463cd1ee9d79dfa0f45a9bd6f34525af08
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-05-11 18:36:22 +0100

    React Login implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Login/>
       <Outlet/>
       {/* <div className="test-div">
         <LocationCard place={{id:0,title:"Sitio de teste Spot fixe", location:"Lisboa",reviewNumber:4,rating:8,description:"Sitio calmo",image:"https://www.cm-amadora.pt/images/DESPORTO_LAZER/PARQUES_JARDINS/PARQUE_FANTASIA/1.jpg"}}></LocationCard>
---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { FormEvent, useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleLogin(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:8000/login/`, { username, password })
+      .then((resp) => {
+        setMsg(resp.data);
+      })
+      .catch((error) => setError(error));
+  }
+  return (
+    <section>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      {<p>{msg}</p>}
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </section>
+  );
+}
+
+export default Login;
