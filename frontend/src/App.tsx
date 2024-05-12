7bbcfc433bc3f47f31ce1e1fc6777fb0c8dec744
AndreOD <115731734+AndreOD@users.noreply.github.com>
2024-05-12 03:11:14 +0100

    Main page html made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
+
+  
+
+
   return (
     <div>
       <Header/>
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: 'https://localhost:3000/api/',
-    timeout: 1000,
-    headers: {'Content-Type': 'application/json'}
-  });
+const api = axios.create({
+  baseURL: 'https://localhost:3000/api/',
+  headers: {'Content-Type': 'application/json'}
+});
+
+export default api
